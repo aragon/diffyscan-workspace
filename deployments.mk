46ad8e29f9ec5fc08e55f3ba8b2ce0756a3999f2
Jør∂¡ <itsbrickpop@gmail.com>
2025-03-27 11:35:16 +0000

    Adding Holesky

--- deployments.mk
+++ deployments.mk
@@ -1,17 +1,17 @@
 SEPARATOR := ""
 
 # Mainnet's
-MAINNET_DEPLOYMENTS := osx-mainnet admin-mainnet multisig-mainnet spp-mainnet token-voting-mainnet
-POLYGON_DEPLOYMENTS := osx-polygon admin-polygon multisig-polygon spp-polygon token-voting-polygon
-BASE_DEPLOYMENTS := osx-base admin-base multisig-base spp-base token-voting-base
-ARBITRUM_DEPLOYMENTS := osx-arbitrum admin-arbitrum multisig-arbitrum spp-arbitrum token-voting-arbitrum
-ZKSYNC_DEPLOYMENTS := osx-zksync admin-zksync multisig-zksync spp-zksync token-voting-zksync
+MAINNET_DEPLOYMENTS := osx-mainnet admin-mainnet multisig-mainnet token-voting-mainnet
+POLYGON_DEPLOYMENTS := osx-polygon admin-polygon multisig-polygon token-voting-polygon
+BASE_DEPLOYMENTS := osx-base admin-base multisig-base token-voting-base
+ARBITRUM_DEPLOYMENTS := osx-arbitrum admin-arbitrum multisig-arbitrum token-voting-arbitrum
+ZKSYNC_DEPLOYMENTS := osx-zksync admin-zksync multisig-zksync token-voting-zksync
 
 # Testnet's
 
-SEPOLIA_DEPLOYMENTS := osx-sepolia admin-sepolia multisig-sepolia spp-sepolia token-voting-sepolia
-ZKSYNC_SEPOLIA_DEPLOYMENTS := osx-zksyncsepolia admin-zksyncsepolia multisig-zksyncsepolia spp-zksyncsepolia token-voting-zksyncsepolia
-HOLESKY_DEPLOYMENTS :=
+SEPOLIA_DEPLOYMENTS := osx-sepolia admin-sepolia multisig-sepolia token-voting-sepolia
+HOLESKY_DEPLOYMENTS := osx-holesky admin-holesky multisig-holesky token-voting-holesky
+ZKSYNC_SEPOLIA_DEPLOYMENTS := osx-zksyncsepolia admin-zksyncsepolia multisig-zksyncsepolia token-voting-zksyncsepolia
 
 AVAILABLE_DEPLOYMENTS := \
 	$(MAINNET_DEPLOYMENTS) $(SEPARATOR) \
